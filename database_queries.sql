b2aec8690f1c906dc073f1a4b23aa3a332311655
Gena Israel <gena.catharine@gmail.com>
2017-09-22 10:09:14 -0600

    finished

--- database_queries.sql
+++ database_queries.sql
@@ -8,10 +8,11 @@ SELECT first_name FROM users WHERE first_name IN ('Kyle', 'Danny');
 
 SELECT user_id, count (*) as number from contact group by user_id;
 
-* Add a field to the `User` table called `is_human` with the appropriate data type
-INSERT 
+ALTER TABLE users, ADD COLUMN is_human data_type boolean;
 * Set `is_human` for all Users except Danny to `true`
-
+UPDATE users set is_human = true where id != 3;
+UPDATE users set is_human = false where id =3;
 * Delete all human users
-
+DELETE FROM users where is_human = true;
 * Drop the `Contact` table
+DROP table contact;
